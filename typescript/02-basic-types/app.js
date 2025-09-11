1eb0912c53ece6270bb7f5516a55d22360829a59
TEKim <kte9411@gmail.com>
2025-09-11 15:42:45 +0900

    [TS] 2025-09-11 타입스크립트 기초 실습

---
+++ typescript/02-basic-types/app.js
@@ -0,0 +1,5 @@
+var person = {
+    name: "gildong",
+    age: 30,
+};
+console.log(person);
---
+++ typescript/02-basic-types/app.ts
@@ -0,0 +1,11 @@
+// const person: { name: string; age: number } = {
+//   name: "gildong",
+//   age: 30,
+// };
+
+const person = {
+  name: "gildong",
+  age: 30,
+};
+
+console.log(person.name);
